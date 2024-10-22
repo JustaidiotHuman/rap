fa57f8e8e21c3e8b786a47d0ed900fc235ceefcc
JustaidiotHuman <daniel.rami.code@gmail.com>
2024-10-22 11:31:18 +0200

    x

--- Lab1/Lab1.asm
+++ Lab1/Lab1.asm
@@ -1,6 +1,6 @@
 ;---------------------------------------------------------------
 ; Lab1.asm - Mupltiplizieren und ausgeben einer Zahl in Hexformat
-; Skalarprodukt mit Schleife berechnen (Refined version with newline fix)
+; Skalarprodukt mit Schleife berechnen (version with newline fix)
 ; Author: DR
 ; ---------------------------------------------------------------
 
@@ -18,47 +18,45 @@ section .text
     global main
 
 main:
-
     push ebp
     mov ebp, esp
 
     ; --Mulitplication and output of a*b--
-    mov eax, [a]            ; Load a into eax
-    imul eax, [b]           ; Multiply eax by b (a * b)
-    push eax                ; Push result onto the stack
-    push hexformat          ; Push hex format string onto the stack
-    call printf             ; Call printf to print hex result
-    add esp, 8              ; Clean up the stack (2 arguments)
-
+    mov eax, [a]            ; load a into eax
+    imul eax, [b]           ; multiply eax by b (a * b)
+    push eax                ; push result onto the stack
+    push hexformat          ; set hex format string
+    call printf             ; print 42 in hex
+    add esp, 8              ; clean up the stack (2 arguments)
 
     ; --Calculate Scalar Product in Loop--
-    xor eax, eax            ; Clear eax (accumulator for scalar product)
-    mov ecx, 0              ; Initialize loop counter
+    xor eax, eax            ; clear eax (for later use)
+    mov ecx, 0              ; initialize loop counter
 
 loop_start:
     ; Check if loop is done (3 vector components)
-    cmp ecx, 3              ; Compare loop counter with 3
-    je loop_end             ; If ecx == 3, exit the loop
+    cmp ecx, 3              ; compare loop counter with 3 --sets zero flag
+    je loop_end             ; if ecx == 3, exit the loop
 
     ; Load x[ecx] and y[ecx], multiply them
-    mov edx, [x + ecx * 4]  ; Load x[ecx] into edx
-    imul edx, [y + ecx * 4] ; Multiply edx with y[ecx]
-    add eax, edx            ; Add result to eax (accumulator)
+    mov edx, [x + ecx * 4]  ; load x[ecx] into edx (ecx * 4 for 4 byte integers offset)
+    imul edx, [y + ecx * 4] ; multiply edx with y[ecx]
+    add eax, edx            ; add result to eax (summe)
 
     ; Increment loop counter
-    inc ecx                 ; Increment counter
-    jmp loop_start          ; Repeat the loop
+    inc ecx
+    jmp loop_start
 
 loop_end:
-    ; ----------- Print Scalar Product -----------
-    push eax                ; Push scalar product onto the stack
-    push decformat          ; Push decimal format string onto the stack
-    call printf             ; Call printf to print the scalar product
-    add esp, 8              ; Clean up the stack (2 arguments)
+    ; --Print Scalar Product--
+    push eax                ; push scalar product onto the stack
+    push decformat          ; set decimal format string
+    call printf             ; print scalar product
+    add esp, 8              ; clean up the stack (2 arguments)
 
-    ; ----------- Exit Program -----------
-    mov eax, 0              ; Return 0
+    ; --Exit--
+    mov eax, 0              ; return 0
     mov esp, ebp
     pop ebp
-    ret                     ; Return from main
+    ret                     ; return from main
 
